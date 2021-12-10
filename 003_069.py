48572ff752e2b6170e0fd4bd471dfd1b25c552db
alghaz <alghaz.h@gmail.com>
2021-12-10 11:21:15 +0700

    nambahin lingkaran

--- 003_069.py
+++ 003_069.py
@@ -9,20 +9,36 @@ Materi  : Membuat animasi  dengan turtle mainloop
 
 import turtle #memanggil library turtle
 
+emotion = input('pilih pola? (s = spiral, l = lingkaran) ')
+
 win = turtle.Screen() #memunculkan window turtle
 win.bgcolor("lightblue") #window color
-win.title("Tugas Animation dengan Mainlooop") #Judul window
-win.setup(width=700, height=700) #ukuran window
+win.title("Tugas Animation dengan Mainlooop\n") #Judul window
+win.setup(width=700, height=700) #ukuran windowl
 
-t = turtle.Turtle() #memasukkan library turtle kedalam variable t
-t.speed(0)
-temp=1
-clrlist=["red","green","red","blue"]
-for i in range(400):
-    t.color(clrlist[i%4])
-    t.forward(temp)
-    t.left(120)
-    t.left(1)
-    temp=temp+1
-turtle.mainloop()
-turtle.done()
+if emotion == 's':
+    t = turtle.Turtle() #memasukkan library turtle kedalam variable t
+    t.speed(0)
+    temp=1
+    clrlist=["red","green","red","blue"]
+    for i in range(400):
+        t.color(clrlist[i%4])
+        t.forward(temp)
+        t.left(120)
+        t.left(1)
+        temp=temp+1
+    turtle.mainloop()
+    turtle.done()
+else:
+    turtle.bgcolor("black")
+    turtle.pensize(2)
+    turtle.speed(0)
+
+    for i in range(6):
+        for colours in ["red","magenta","blue","cyan","green","yellow","white"]:
+            turtle.color(colours)
+            turtle.circle(100)
+            turtle.left(10)
+    turtle.hideturtle()
+    
+
